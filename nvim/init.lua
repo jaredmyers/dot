006c0fc46c13c835359f15fdac7e94ff41226de7
Jared Myers <jaredjmyers@gmail.com>
2023-05-06 03:43:31 -0400

    update tmux conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,5 +14,5 @@ vim.cmd "set incsearch"
 
 vim.cmd "let g:go_auto_type_info=1"
 
---vim.cmd "let g:lightline = {'colorscheme': 'one'}"
-
+--vim.cmd "let g:python3_host_prog = '$HOME/langs/python/bin/python3.11'"
+--vim.cmd "let g:go_bin_path = '$HOME/langs/go/bin'"
