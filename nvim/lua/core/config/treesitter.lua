b83d50df9e759c3a229dfb5bdf62ef1fccf477c5
jared <jaredjmyers@gmail.com>
2024-06-08 18:00:38 -0400

    remove treesitter latex

--- nvim/lua/core/config/treesitter.lua
+++ nvim/lua/core/config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "go", "python", "yaml", "json", "hcl", "bash", "html", "css", "dockerfile", "dot", "javascript", "typescript", "jq", "latex", "markdown", "perl", "php", "sql", "cpp", "make", "toml", "markdown", "markdown_inline"},
+  ensure_installed = { "c", "lua", "rust", "go", "python", "yaml", "json", "hcl", "bash", "html", "css", "dockerfile", "dot", "javascript", "typescript", "jq", "markdown", "perl", "php", "sql", "cpp", "make", "toml", "markdown", "markdown_inline"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
